10602574cb0d1cbf1e29bb930b7fc7d44e64b473
mahima-git <maahi0195@gmail.com>
2019-12-12 21:14:30 -0800

    dhfhdefhdf

--- addition_of_numbr.py
+++ addition_of_numbr.py
@@ -3,7 +3,7 @@ purpose : Simple two number addition progra
 Author : mahima choudhary
 Date: 11 dec 2019
 """
-a= 7
-b=4
+a=int(input("enter first value : "))
+b=int(input("enter second value :")) 
 c=a+b
 print("addition  of a+b is ",c)
